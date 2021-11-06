373aaad5b54df4ac4601e1cf2ba15fee89460bcb
smshack <5432tat@naver.com>
2021-11-06 13:48:53 +0900

    3.3.2 props 사용 클래스 사용

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,31 +1,23 @@
-import React from 'react'
+import React,{Component}from 'react'
 import PropTypes from 'prop-types'
-// const  MyComponent =(props)=> {
-//     const {name,children} = props
-//     return (
-//         <div>
-//              나의 새롭고 멋진 컴포넌트{name} <br />
-//              children 의 값은 {children}
-//         </div>
-//     )
-// }
 
-const  MyComponent =({name,favoriteNumber,children})=> {
-    return (
+class MyComponent extends Component{
+    static defaultProps = {
+        name:'기본이름'
+    }
+    static propTypes ={
+        name:PropTypes.string,
+        favoriteNumber:PropTypes.number.isRequired
+    }
+    render(){
+        const {name, favoriteNumber, children} = this.props;// 비구조화 할당
+    return(
         <div>
              나의 새롭고 멋진 컴포넌트{name} <br />
              children 의 값은 {children} <br />
              제가 좋아하는 숫자는 {favoriteNumber}
         </div>
     )
-}
-// 기본값 설정
-MyComponent.defaultProps = {
-    name:'기본 이름'
-}
-// 타입 지정
-MyComponent.prototype ={
-    name:PropTypes.string,
-    favoriteNumber:PropTypes.number.isRequired
+    }
 }
 export default MyComponent
